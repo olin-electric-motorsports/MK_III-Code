6d38b8523bcea3f66c8376c553f7955889a2751b
ljordan51 <ljordan51@gmail.com>
2018-05-20 20:59:06 -0400

    thinned out AIR code to simply run precharge sequence, tested with LEDs and it appeared to work, when tested with relays it seemed to also work when checking continuity across AIR+ before and after sequence completion but for some reason the tractive system did not take any time to charge, it seemed to immediately jump to the maximum voltage

--- boards/AIR/AIR.c
+++ boards/AIR/AIR.c
@@ -1,164 +1,95 @@
-/**********************************************************
-This is the base document for getting started with writing
-firmware for OEM.
-TODO delete before submitting
-**********************************************************/
-
 /*
-Header:
-    Explain what this project does as overview
-Author:
-    @author
+Header: Basic precharge implementation for AIR Control Board. This code has two basic states sensed through the TS- AIR auxillary contacts.
+The first is when the TS- AIR is open (the shutdown circuit is not complete). In this state, the output pins which low-side-drive (LSD) the
+precharge and TS+ AIR are set low and the precharge counter (which handles the length of the precharge sequence) is constantly reset to 0x00.
+The second is when the TS- AIR is closed (the shutdown circuit is complete). In this state, the precharge LSD is immediately pulled high,
+beginning the precharge sequence. An overflow interrupt on timer0 counts out 13 seconds (set by precharge_threshold). Once this time is exceeded,
+the TS+ AIR LSD is pulled high and a CAN message should be sent stating that precharge is complete.
+
+Author: Lucky Jordan
 */
 
 /*----- Includes -----*/
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include "can_api.h"
 
-/*----- Macro Definitions -----*/
-// LEDs
-#define EXT_LED_ORANGE              PB0
-#define PORT_EXT_LED_ORANGE         PORTB
-#define LED1_PIN                    PD0
-#define LED2_PIN                    PB1
-#define PORT_LED1                   PORTD
-#define PORT_LED2                   PORTB
-
-
-// CAN Positions
-#define MAIN_TRACTIVE               2
-
-
-// gFlag positions
-#define SHUTDOWN_COMPLETE           1
-
-#define UPDATE_STATUS               0
-
-
 /*----- Global Variables -----*/
-volatile uint8_t gFlag = 0x00;  // Global Flag
-uint8_t gCAN_MSG[8] = {0, 0, 0, 0, 0, 0, 0, 0};  // CAN Message
-uint8_t can_recv_msg[8] = {};
+volatile uint8_t timer_ovf_count = 0x00; //This variable is incremented on each timer overflow to give a measure of time passed
+uint8_t precharge_threshold = 200; //This variable is a threshold at which we consider precharge complete when timer_ovf_count exceeds it
+//Math for above precharge_threshold
+//The timer is configured with a clock prescaler of 1024 in timer0_setup. This means that the clock cycles will increment by 1 each time
+//1024 cycles pass. In this case, the clock frequency is set to 4MHz; therefore, we should see 4000000/1024 increments per second.
+//Because the timer is 8 bit, it can only count up to 255 cycles and therefore overflows (4000000/1024)/255 times per second. To get the
+//number of overflows in 13 seconds we multiply this value by 13: 13*((4000000/1024)/255) = 199 ish. We round up to 200 to get the threshold.
 
-// Timer counters
-uint8_t clock_prescale = 0x00;  // Used for update timer
+/*----- Macro Definitions -----*/
+// On board LEDs
+#define LED1		PB0
+#define LED2		PB1
 
+// External LEDs
+#define EXT_LED1	PD0
+#define EXT_LED2	PC0
 
-/*----- Interrupt(s) -----*/
-// *pg 76 of datasheet*
-ISR(CAN_INT_vect) {
-    // Check first board (Dashboard)
-    CANPAGE = (0 << MOBNB0);
-    if(bit_is_set(CANSTMOB, RXOK)) {
-        volatile uint8_t msg = CANMSG;      //grab the first byte of the CAN message
-        can_recv_msg[0] = msg;
-        can_recv_msg[1] = CANMSG;
-        // uint8_t start_button = CANMSG;
-        // can_recv_msg[0] = msg;
-        // can_recv_msg[1] = 0x99;
+// Precharge & AIR
+#define PRECHARGE_LSD   PC4 //Pin to low side drive precharge relay
+#define AIR_LSD         PC5 //Pin to low side drive AIR +
+#define AIR_auxillary   PC6 //PIn to sense state of AIR - auxillary contacts
 
-        if(can_recv_msg[1] == 0xFF) {
-            gFlag |= SHUTDOWN_COMPLETE;           //trip flag
-        } else {
-            gFlag &= ~SHUTDOWN_COMPLETE;          //reset flag
-        }
+// CAN Message Objects
+#define MOB_BROADCAST 0 //Broadcasts precharge sequence complete
 
+// CAN Message
+uint8_t gCANMessage[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-        //Setup to Receive Again
-        CANSTMOB = 0x00;
-        CAN_wait_on_receive(0, CAN_ID_DASHBOARD, CAN_LEN_DASHBOARD, 0xFF);
-    }
-
+//Setup 8 bit timer
+//helpful reference http://exploreembedded.com/wiki/AVR_Timer_programming
+void timer0_setup(){
+  sei(); //enable interrupts
+  // Timer0 Overflow Interrupt Enabled
+  TIMSK0 |= _BV(TOIE0);
+  // Timer with 1024 prescaler
+  TCCR0B |= _BV(CS02) | _BV(CS00);
 }
 
-ISR(PCINT0_vect) {
-    /*
-    Standard Pin Change Interupt
-    Covers Interrupts:
-    */
+ISR(TIMER0_OVF_vect){
+  timer_ovf_count++; //Increment overflow count each time overflow interrupt is triggered
 }
 
-ISR(TIMER0_COMPA_vect) {
-    // Only send CAN msgs every 20 cycles
-    if(clock_prescale > 20) {
-        gFlag |= _BV(UPDATE_STATUS);
-        clock_prescale = 0;
-    }
-    clock_prescale++;
-}
-
-
-/*----- Functions -----*/
-void initTimer(void) {
-    TCCR0A = _BV(WGM01);    // Set up 8-bit timer in CTC mode
-    TCCR0B = 0x05;          // clkio/1024 prescaler
-    TIMSK0 |= _BV(OCIE0A);
-    OCR0A = 0xFF;
-}
-
-void checkShutdownState(void)   {
-    /*
-    -Check if bits are set
-        -IF they are, set CAN list position to 0xFF
-        -ELSE do set CAN list position to 0x00
-    */
-}
-
-void updateStateFromFlags(void) {
-    /*
-    Based off the state of the flag(s), update components and send CAN
-    */
-    if(bit_is_set(gFlag, SHUTDOWN_COMPLETE)) {
-        gCAN_MSG[MAIN_TRACTIVE] = 0xFF;
-    } else {
-        gCAN_MSG[MAIN_TRACTIVE] = 0x00;
-    }
-}
-
-//TODO any other functionality goes here
-
-
-/*----- MAIN -----*/
 int main(void){
-    /*
-    -Set up I/O
-    -Set up CAN timer (done)
-    -Initialize external libraries (if applicable)
-    -Wait on CAN
-    -Infinite loop checking shutdown state!
-    */
+  // need to enable can i think here
 
-    sei();                  // Enable Interrupts
+  timer0_setup();
 
+  DDRB |= _BV(LED1) | _BV(LED2); //Set on board programming LEDs as uptputs
+  DDRC |= _BV(PRECHARGE_LSD) | _BV(EXT_LED2) | _BV(AIR_LSD); //Set precharge lsd, air + lsd and one external led as output
+  DDRD |= _BV(EXT_LED1); //Set other external led as output
 
-    // CAN Enable
-    CAN_init(CAN_ENABLED);
-    CAN_wait_on_receive(0, CAN_ID_DASHBOARD, CAN_LEN_DASHBOARD, 0xFF);
+  DDRC &= ~_BV(AIR_auxillary); //Set auxillary air - contact sense as input
 
-    initTimer();
+  PORTC &= ~_BV(PRECHARGE_LSD) & ~_BV(AIR_LSD); //Make sure precharge and AIR + relays are open to start
 
-    while(1) {
-        if(bit_is_set(gFlag, UPDATE_STATUS)) {
-            PORT_EXT_LED_ORANGE ^= _BV(EXT_LED_ORANGE);     // Blink Orange LED for timing check
-            PORT_LED1 ^= _BV(LED1_PIN);
-
-            // updateStateFromFlags();
-
-            // if(bit_is_set(gFlag, BRAKE_PRESSED) && bit_is_set(gFlag, TSMS_CLOSED)) {
-            //     CAN_transmit(0, CAN_ID_DASHBOARD, CAN_LEN_DASHBOARD, gCAN_MSG);
-            // }
-        }
-
-        // Do continuously for now
-        updateStateFromFlags();
-
-        if(bit_is_set(gFlag, SHUTDOWN_COMPLETE)) {
-            PORT_LED2 ^= _BV(LED2_PIN);
-            CAN_transmit(0, CAN_ID_AIR_CONTROL, CAN_LEN_AIR_CONTROL, gCAN_MSG);
-        }
+  while(1){
+    //Should I change this to interrupts? not sure, for loop seems like it might work fine but also this board does other stuff that I haven't implemented yet
+    //If AIR - is closed (pin is high), start precharge sequence (pull precharge lsd high)
+    if (bit_is_set(PINC, AIR_auxillary)) {
+      // do precharge
+      if (timer_ovf_count < precharge_threshold) {
+        PORTC |= _BV(PRECHARGE_LSD);
+        PORTD |= _BV(EXT_LED1);
+      } else {
+        PORTC |= _BV(AIR_LSD);
+        PORTC |= _BV(EXT_LED2);
+      }
+    } else { //otherwise set both relays LSDs low (open the relays) and reset overflow count
+      PORTC &= ~_BV(PRECHARGE_LSD) & ~_BV(AIR_LSD);
+      timer_ovf_count = 0x00;
     }
+    //Send CAN message saying precharge is done, car can then be started by dashboard so throttle can start sending torque requests to motor controller
+  }
 }
